5d9b3eeba89d90ac416130cc7189f61c9f49f913
root <root@yuno.net>
2020-05-19 15:41:52 +0000

    target group changes

--- infra/alb.tf
+++ infra/alb.tf
@@ -40,8 +40,20 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 8080
       target_type      = "ip"
+      health_check = {
+        enabled             = true
+        interval            = 20
+        path                = "/"
+        port                = "traffic-port"
+        healthy_threshold   = 5
+        unhealthy_threshold = 2
+        timeout             = 10
+        protocol            = "HTTP"
+        matcher             = "200"
+      }
     }
   ]
+
 
   http_tcp_listeners = [
     {
